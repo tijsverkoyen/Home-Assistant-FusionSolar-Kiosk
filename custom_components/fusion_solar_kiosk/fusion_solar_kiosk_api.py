4f0c662bba1738d764b25aaa89993f0a8154cc2d
Anders Hoglund <AndWho@gmail.com>
2022-08-12 10:14:14 +0200

    Added validation of lifetime energy, fixing issue #28

--- custom_components/fusion_solar_kiosk/fusion_solar_kiosk_api.py
+++ custom_components/fusion_solar_kiosk/fusion_solar_kiosk_api.py
@@ -8,17 +8,23 @@ from .const import (
     ATTR_FAIL_CODE,
     ATTR_SUCCESS,
     ATTR_DATA_REALKPI,
+    ATTR_TOTAL_LIFETIME_ENERGY,
 )
 
 from requests import get
 
 _LOGGER = logging.getLogger(__name__)
 
+lastGoodLifetimeEnergy = {}
+
 class FusionSolarKioksApi:
     def __init__(self, host):
         self._host = host
 
     def getRealTimeKpi(self, id: str):
+
+        global lastGoodLifetimeEnergy
+
         url = self._host + '/rest/pvms/web/kiosk/v1/station-kiosk-file?kk=' + id
         headers = {
             'accept': 'application/json',
@@ -36,6 +42,17 @@ class FusionSolarKioksApi:
             jsonData[ATTR_DATA] = json.loads(html.unescape(jsonData[ATTR_DATA]))
             _LOGGER.debug('Received data for ' + id + ': ')
             _LOGGER.debug(jsonData[ATTR_DATA][ATTR_DATA_REALKPI])
+
+            # Validate response
+            if id not in lastGoodLifetimeEnergy:
+                lastGoodLifetimeEnergy[id] = 0
+
+            if jsonData[ATTR_DATA][ATTR_DATA_REALKPI][ATTR_TOTAL_LIFETIME_ENERGY] < lastGoodLifetimeEnergy[id]:
+                jsonData[ATTR_DATA][ATTR_DATA_REALKPI][ATTR_TOTAL_LIFETIME_ENERGY] = lastGoodLifetimeEnergy[id]
+                _LOGGER.debug(jsonData[ATTR_DATA][ATTR_DATA_REALKPI])
+            else:
+                lastGoodLifetimeEnergy[id] = jsonData[ATTR_DATA][ATTR_DATA_REALKPI][ATTR_TOTAL_LIFETIME_ENERGY]
+
             return jsonData[ATTR_DATA][ATTR_DATA_REALKPI]
 
         except FusionSolarKioskApiError as error:
